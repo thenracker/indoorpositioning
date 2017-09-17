5e59efb5467eb587f7d0a06e6434a7b6636f482c
Petr Weissar <weissar.petr@gmail.com>
2017-09-17 20:32:34 +0200

    Pokus o zapisování pozice s útlem atd, konečně využití kvaternionu

---
+++ app/src/main/java/cz/weissar/indoorpositioning/remaster/utils/VectorHolder.java
@@ -0,0 +1,123 @@
+package cz.weissar.indoorpositioning.remaster.utils;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+
+import cz.weissar.indoorpositioning.old.transforms.Quat;
+
+/**
+ * Created by petrw on 17.09.2017.
+ */
+
+public class VectorHolder implements SensorEventListener {
+
+    float posX, posY, posZ;
+
+    float lastTimestamp;
+    float x, y, z;
+
+    float speedX, speedY, speedZ;
+    final static float DAMPING = 0.001f; //tlumení za 1ms
+    final static float SPEED_GROW = 1.000f; //síla růstu rychlosti za 1 jednotku přetížení (zatim necháme 1)
+
+    /**
+     * Could be used when a step is detected - the speed should stay the same
+     */
+    public void startNewEpisode(){
+        posX = posY = posZ = 0;
+    }
+
+    /**
+     * @param values Values from TYPE_ROTATION_VECTOR ONLY!!!
+     * @return returns float[3] of XYZ world coordinations (+Y to North, +X to East, -Z to middle Earth (gravity pole))
+     */
+    public float[] getRealWorldMove(float[] values){ //float[]{x, y, z, θ]
+        float[] rotMat = new float[9];
+        SensorManager.getRotationMatrixFromVector(rotMat, values);
+
+        float[] realPos = new float[3]; // = mat * vec;
+
+        realPos[0] = rotMat[0] * posX + rotMat[1] * posY + rotMat[2] * posZ;
+        realPos[1] = rotMat[3] * posX + rotMat[4] * posY + rotMat[5] * posZ;
+        realPos[2] = rotMat[6] * posX + rotMat[7] * posY + rotMat[8] * posZ;
+
+        return realPos;
+    }
+
+    /**
+     * ACCELERATION a = deltaPosition / deltaTime
+     */
+
+    /**
+     * Reads 3 dimensions and crops noise
+     *
+     * @param timeStamp
+     * @param data      float[3]
+     */
+    private void addAcceleration(long timeStamp, float... data) {
+        cropNoise(data);
+        //cropByGyro??
+        computeSpeed(timeStamp, data[0], data[1], data[2]);
+        computePosition();
+        damp();
+    }
+
+    //teoreticky addGyro? Na eliminaci vzniku akcelerace rotací???
+
+    private void computeSpeed(long timeStamp, float x, float y, float z) {
+
+        float m = lastTimestamp - timeStamp;
+        speedX += x * m * SPEED_GROW;
+        speedY += y * m * SPEED_GROW;
+        speedZ += z * m * SPEED_GROW;
+
+        this.lastTimestamp = timeStamp;
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    private void computePosition() {
+        //teoreticky by měla mít pozice nějakou prodlevu - takhle kopíruje speedXYZ - uvidíme časem
+        posX += speedX;
+        posY += speedY;
+        posZ += speedZ;
+    }
+
+    /**
+     * Crops sensor noise
+     *
+     * @param data float[n]
+     */
+    private void cropNoise(float... data) {
+        for (int i = 0; i < data.length; i++) {
+            if (Math.abs(data[i]) < 0.5) {
+                data[i] = 0;
+            } else {
+                data[i] = Math.round(data[i] * 1000f) / 1000f;
+            }
+        }
+    }
+
+    /**
+     * Simulates damp of speed
+     */
+    private void damp() {
+        speedX = speedX > 0 ? speedX - DAMPING : speedX + DAMPING;
+        speedY = speedY > 0 ? speedY - DAMPING : speedY + DAMPING;
+        speedZ = speedZ > 0 ? speedZ - DAMPING : speedZ + DAMPING;
+    }
+
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
